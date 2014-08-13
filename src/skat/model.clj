67d8ba834279d2851980af1f226e6c05abb80ac4
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2014-08-13 21:24:41 +0200

    Fixed naming

--- src/skat/model.clj
+++ src/skat/model.clj
@@ -8,26 +8,26 @@
 (def color-ordinal "Colors' ordinals"
   { :kreuz 4 :grun 3 :herz 2 :schell 1 })
 
-;;; Values
+;;; Figures
 
-(def values "Possible cards' values"
+(def figures "Possible cards' figures"
   #{ :7 :8 :9 :10 :W :Q :K :A })
-(def value-ordinal-normal "Values' ordnals in normal games"
+(def figure-ordinal-normal "Figures' ordnals in normal games"
   { :7 0 :8 1 :9 3 :Q 4 :K 5 :10 6 :A 7 :W 8 })
-(def value-ordinal-null "Values' ordnals in null games"
+(def figure-ordinal-null "Figures' ordnals in null games"
   { :7 0 :8 1 :9 3 :10 7 :W 4 :Q 5 :K 6 :A 8 })
-(def value-points "Points for each card value"
+(def figure-values "Values for each card figure"
   { :7 0 :8 0 :9 0 :W 2 :Q 3 :K 4 :10 10 :A 11 })
 
 ;;; Cards
 
-(defrecord Card [color value])
+(defrecord Card [color figure])
 (defn property-matches? "Whether card's property as given" [n v c]
   (identical? (n c) v))
 (defn compare-by-color-normal "Order cards by color in normal game"
   [c1 c2]
   (letfn [(W? [c]
-            (property-matches? :value :W c))
+            (property-matches? :figure :W c))
           (W-exceptonal-ordinal [c]
             (if (W? c)
               (inc (:kreuz color-ordinal))
@@ -42,30 +42,30 @@
     (let [o1 (ordinal c1)
           o2 (ordinal c2)]
       (compare o1 o2))))
-(defn compare-by-value [fun c1 c2]
-  (let [o1 ((:value c1) fun)
-        o2 ((:value c2) fun)]
+(defn compare-by-figure [fun c1 c2]
+  (let [o1 ((:figure c1) fun)
+        o2 ((:figure c2) fun)]
     (compare o1 o2)))
-(def compare-by-value-normal "Order cards by value in normal game"
-  (partial compare-by-value value-ordinal-normal))
-(def compare-by-value-null "Order cards by value in null game"
-  (partial compare-by-value value-ordinal-null))
-(defn compare-for-sort "Compose comparator of cards for values and colors"
-  [compare-by-value compare-by-color]
+(def compare-by-figure-normal "Order cards by figure in normal game"
+  (partial compare-by-figure figure-ordinal-normal))
+(def compare-by-figure-null "Order cards by figure in null game"
+  (partial compare-by-figure figure-ordinal-null))
+(defn compare-for-sort "Compose comparator of cards for figures and colors"
+  [compare-by-color compare-by-figure]
   (fn [c1 c2]
     (if (identical? (:color c1) (:color c2))
-      (compare-by-value c1 c2)
+      (compare-by-figure c1 c2)
       (compare-by-color c1 c2))))
-(defn filter-cards [property-name property-value cards]
-  (filter (partial property-matches? property-name property-value) cards))
+(defn filter-cards [property-name property-figure cards]
+  (filter (partial property-matches? property-name property-figure) cards))
 (def filter-color "Filter cards by color"
   (partial filter-cards :color))
-(def filter-value "Filter cards by value"
-  (partial filter-cards :value))
+(def filter-figure "Filter cards by figure"
+  (partial filter-cards :figure))
 (def deck "Complete deck of cards (unsorted)"
   (letfn [(cards-of-color [c]
-            (let [cards-of-value #(Card. c %)]
-              (helpers/list-from cards-of-value values)))
+            (let [cards-of-figure #(Card. c %)]
+              (helpers/list-from cards-of-figure figures)))
           (cards-grouped-by-color []
             (helpers/list-from cards-of-color colors))]
     (flatten (cards-grouped-by-color))))
