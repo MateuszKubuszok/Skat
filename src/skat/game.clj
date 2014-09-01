cfc621700192bb54b14be4495fe68cf33d80e211
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2014-09-01 18:18:53 +0200

    Sketches of knowledge functions

--- src/skat/game.clj
+++ src/skat/game.clj
@@ -17,8 +17,7 @@
 
 ;;; Knowledge
 
-(defrecord Knowledge [cards-played cards-owned])
-(defn add-knowledge [knowledge turn c1 c2 c3] knowledge); TODO: calc knowledge
+(defrecord PlayerKnowledge [self cards-played cards-owned])
 
 ;;; Players
 
@@ -27,20 +26,44 @@
 ;;; Turn
 
 (defrecord Turn [players knowledge order])
-(defn next-players [order winner]
+
+;;; Knowledge update
+
+(defn update-cards [players-cards fun]
+  (let [players (keys players-cards)]
+    (zipmap players (map fun players))))
+
+(defn update-cards-played [cards-played played-now]
+  (letfn [(add-card [player] (conj (cards-played player) (played-now player)))]
+    (update-cards cards-played add-card)))
+
+(defn update-cards-owned [cards-owned played-now]
+  (letfn [(used-now? [player card] (= (played-now player) card))
+          (remove-card [player] (remove (partial used-now? player) (cards-owned player)))]
+    (update-cards cards-owned remove-card)))
+
+(defn update-knowledge [knowledge turn played-now]
+  (-> knowledge
+    (update-in [1 :cards-played] update-cards-played played-now)
+    (update-in [1 :cards-owned] update-cards-owned played-now)))
+
+;;; Turn update
+
+(defn next-turn-order [order winner]
   (let [{:keys [p1 p2 p3]} order]
     (if (= p1 winner) order
       (if (= p2 winner) { :p1 p2 :p2 p3 :p3 p1 }
         (if (= p3 winner) { :p1 p3 :p2 p1 :p3 p2 })))))
 (defn next-turn [turn winner]
   (let [{:keys [order]} turn]
-    (assoc turn :order (next-players order winner))))
+    (assoc turn :order (next-turn-order order winner))))
 (defn play-turn [config {:keys [turn knowledge]}]
   (let [{:keys [players order]} turn
         {:keys [p1 p2 p3]} order
         c1 ((:play-1st-card-fun p1) config (knowledge p1))
         c2 ((:play-2nd-card-fun p2) config (knowledge p2) c1)
         c3 ((:play-3rd-card-fun p3) config (knowledge p3) c1 c2)
+        played-now { :p1 c1 :p2 c2 :p3 c3 }
         winner ((:who-won? config) c1 c2 c3)]
     { :turn (next-turn turn winner)
-      :knowledge (add-knowledge knowledge turn c1 c2 c3)}))
+      :knowledge (update-knowledge knowledge turn played-now)}))
