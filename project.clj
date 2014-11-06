70ecee8e76ea7463c83822080374f7fcb000a853
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2014-11-06 21:53:39 +0100

    Project cupports Eclipse's CounterClockWise

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
   :profiles {:uberjar {:aot :all}}
   :main skat.core
   :target-path "target/%s"
