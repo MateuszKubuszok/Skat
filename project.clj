cccb5d25dd1fae584e1341cead680fcadf44252b
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2015-03-15 22:44:26 +0100

    All games values calculation, some fixes in normal games calc (with/without skat)

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.trace "0.7.8"]]
   :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
   :profiles {:uberjar {:aot :all}}
--- src/skat/auction.clj
+++ src/skat/auction.clj
@@ -1,5 +1,6 @@
 (ns skat.auction
-  (:require [clojure.set :as sets]
+  (:require [clojure.core.match :refer [match]]
+            [clojure.set :as sets]
             ;[clojure.tools.trace :refer :all]
             [skat.log :as log]
             [skat.cards :as cards]
@@ -30,7 +31,7 @@
 
 ;;; Peaks' values for normal games
 
-(defn peaks-for-trumphs "Number of peaks" [type trumphs cards]
+(defn peaks-for-trumphs "Count peaks by trumphs" [type trumphs cards]
   (let [sorting          (cards/compare-for-sort
                           (cards/compare-by-color-display type)
                           cards/compare-by-figure-normal)
@@ -39,21 +40,21 @@
         considered-cards (filter #(helpers/coll-contains? trumphs %) cards)
         sorted-cards     (sort invert-sorting cards)]
     (peaks-value-calculator sorted-trumphs sorted-cards)))
-(defn peaks-grand "Number of peaks in grand game" [cards]
+(defn peaks-grand "Count peaks in grand game" [cards]
   (let [trumphs (filter cards/trumph-grand? cards/deck)]
     (peaks-for-trumphs :grand trumphs cards)))
-(defn peaks-color "Number of peaks in color game" [color cards]
+(defn peaks-color "Count peaks in color game" [color cards]
   (let [trumphs (filter (partial cards/trumph-color? color) cards/deck)]
     (peaks-for-trumphs color trumphs cards)))
-(defn peaks-kreuz "Number of peaks in kreuz game" [cards]
+(defn peaks-kreuz "Count peaks in kreuz game" [cards]
   (peaks-color :kreuz cards))
-(defn peaks-grun "Number of peaks in grun game" [cards]
+(defn peaks-grun "Count peaks in grun game" [cards]
   (peaks-color :grun cards))
-(defn peaks-herz "Number of peaks in herz game" [cards]
+(defn peaks-herz "Count peaks in herz game" [cards]
   (peaks-color :herz cards))
-(defn peaks-schell "Number of peaks in schell game" [cards]
+(defn peaks-schell "Count peaks in schell game" [cards]
   (peaks-color :schell cards))
-(def peaks "Peaks value calculation functions"
+(def peaks "Peaks' values calculation functions"
   { :grand  peaks-grand,
     :kreuz  peaks-kreuz,
     :grun   peaks-grun,
@@ -65,10 +66,29 @@
 (def game-type-coefficients
   { :grand 24, :kreuz 12, :grun 11, :herz 10, :schell 9 })
 
-(defn normal-game-value [cards type with-skat? ouvert?]
+(defn normal-game-value "Calculate normal game value"
+  [cards type with-skat? ouvert?]
   (let [game-type-coefficient (game-type-coefficients type)
         peaks-type            (peaks type)
         peaks-value           (peaks-type cards)
-        for-skat              (if with-skat? 1 0)
+        for-skat              (if with-skat? 0 1)
         for-ouvert            (if ouvert? 1 0)]
     (* game-type-coefficient (+ 1 peaks-value for-skat for-ouvert))))
+
+;;; Null game values
+
+(def null-game-values
+  { [true  false] 23,
+    [false false] 35,
+    [true  true ] 46,
+    [false true ] 59 })
+
+(defn null-game-value "Calculate null game value" [with-skat? ouvert?]
+  (null-game-values [with-skat? ouvert?]))
+
+;;; Game values
+
+(defn game-value "Calculate overall game value" [cards type with-skat? ouvert?]
+  (match [type]
+    [:null] (null-game-value with-skat? ouvert?)
+    [_]     (normal-game-value cards type with-skat? ouvert?)))
